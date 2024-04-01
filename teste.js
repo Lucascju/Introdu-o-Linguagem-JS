ccd78c5974348c17d5384f2828dfeaa2e8c840f9
Lucas Lima <lucasla.dasilva@gmail.com>
2024-04-01 00:12:45 -0300

    fundamentos-js

---
+++ teste.js
@@ -0,0 +1,15 @@
+/**
+ * Função que realiza a soma de dois números.
+ * @param {number} a - O primeiro número a ser somado.
+ * @param {number} b - O segundo número a ser somado.
+ * @returns {number} - O resultado da soma.
+ */
+function somar(a, b) {
+    return a + b;
+}
+
+// Exemplo de uso da função somar:
+let numero1 = 5;
+let numero2 = 3;
+let resultado = somar(numero1, numero2);
+console.log("A soma de", numero1, "e", numero2, "é igual a", resultado);
